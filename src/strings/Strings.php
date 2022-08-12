37fa7101d5f455850b3fd40a0350f09a3a73fffe
taras-fid <taras.fid@gmail.com>
2022-08-12 13:52:54 +0300

    rewriting string hometask to PSR12 code style

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -6,22 +6,24 @@ class Strings implements  StringsInterface {
 
     public function snakeCaseToCamelCase(string $input): string
     {
-        $snake = '_';
-        $camel = '';
-        return str_replace($snake, $camel, lcfirst(ucwords($input, '_')));
+        return str_replace('_', '', lcfirst(ucwords($input, '_')));
     }
 
     public function mirrorMultibyteString(string $input): string
     {
         $str_arr = explode(' ', $input);
         $r = '';
+
         foreach ($str_arr as $item) {
             $r_in_for = '';
+
             for ($i = mb_strlen($item); $i>=0; $i--) {
                 $r_in_for .= mb_substr($item, $i, 1);
             }
+
             $r .= $r_in_for . ' ';
         }
+
         return mb_substr($r, 0, -1);
     }
 
@@ -29,8 +31,7 @@ class Strings implements  StringsInterface {
     {
         if ($noun[0] === $noun[strlen($noun) - 1]) {
              return ucfirst(substr($noun, 0, -1) . $noun);
-        }
-        else {
+        } else {
             return 'The ' . ucfirst($noun);
         }
     }
