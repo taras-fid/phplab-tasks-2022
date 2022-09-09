3efa66d120559f8ed1c252db048af50f717e6629
taras-fid <taras.fid@gmail.com>
2022-09-10 00:40:45 +0300

    homework-db done

--- src/db/migrations.php
+++ src/db/migrations.php
@@ -1,32 +1,11 @@
 <?php
-/**
- * TODO
- *  Write DPO statements to create following tables:
- *
- *  # airports
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
- *   - code (varchar)
- *   - city_id (relation to the cities table)
- *   - state_id (relation to the states table)
- *   - address (varchar)
- *   - timezone (varchar)
- *
- *  # cities
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
- *
- *  # states
- *   - id (unsigned int, autoincrement)
- *   - name (varchar)
- */
 
 /** @var \PDO $pdo */
 require_once './pdo_ini.php';
 
 // cities
 $sql = <<<'SQL'
-CREATE TABLE `cities` (
+CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`)
@@ -34,6 +13,29 @@ CREATE TABLE `cities` (
 SQL;
 $pdo->exec($sql);
 
-// TODO states
+$sql = <<<'SQL'
+CREATE TABLE IF NOT EXISTS `states` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`)
+);
+SQL;
+$pdo->exec($sql);
 
-// TODO airports
+$sql = <<<'SQL'
+CREATE TABLE IF NOT EXISTS `airports` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`code` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`city_id` INT(10) UNSIGNED NOT NULL,
+	`state_id` INT(10) UNSIGNED NOT NULL,
+	`address` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`timezone` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`city_id`)
+        REFERENCES `cities`(`id`),
+    FOREIGN KEY (`state_id`)
+        REFERENCES `states`(`id`)
+);
+SQL;
+$pdo->exec($sql);
